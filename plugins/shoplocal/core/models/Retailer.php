98c5854c4811456dffdb683a00d8ac13b0e9fede
Luke Towers <git@luketowers.ca>
2025-02-01 10:47:23 -0600

    Add province filter for retailers to backend

--- plugins/shoplocal/core/models/Retailer.php
+++ plugins/shoplocal/core/models/Retailer.php
@@ -117,6 +117,25 @@ class Retailer extends Model
         return $query;
     }
 
+    public function getProvinceOptions(): array
+    {
+        return [
+            "AB" => "Alberta",
+            "BC" => "British Columbia",
+            "MB" => "Manitoba",
+            "NB" => "New Brunswick",
+            "NL" => "Newfoundland and Labrador",
+            "NT" => "Northwest Territories",
+            "NS" => "Nova Scotia",
+            "NU" => "Nunavut",
+            "ON" => "Ontario",
+            "PE" => "Prince Edward Island",
+            "QC" => "Quebec",
+            "SK" => "Saskatchewan",
+            "YT" => "Yukon",
+        ];
+    }
+
     /**
      * Accessor for $this->logo_url
      */
